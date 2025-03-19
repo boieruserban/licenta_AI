4ee9ccb6348b4ac2fd3c5901aecfeab65e962c2a
Alexandru-Cristian Lodin <alexandru.lodin@gmail.com>
2025-03-19 20:39:39 +0200

    Add individual files for AI sections

---
+++ src/train.py
@@ -0,0 +1,24 @@
+import os
+import queue
+import threading
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+# from torch.optim.lr_scheduler import StepLR
+# from torch.utils.data import DataLoader, Dataset, random_split
+
+# from torchvision import transforms
+from transformers import CLIPProcessor, CLIPModel
+
+from datasets import load_dataset
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox, DISABLED, NORMAL
+
+
+def main():
+    print('Taining NN model ...')
+
+
+if __name__ == '__main__':
+    main()
